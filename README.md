946f86e6da019e0b215b51ad8cab9e37f0f4fc2c
Ken Burkhardt <ken.burkhardt@gmail.com>
2014-06-30 13:28:28 -0400

    latest code

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ SlowCheetahify
 ==============
 
 A sample project to demonstrate how to use the Xml Transforms of the Slow Cheetah tool.
+
+Remember to Download the Slow Cheetah Extension: http://visualstudiogallery.msdn.microsoft.com/69023d00-a4f9-4a34-a6cd-7e854ba318b5
---
+++ SlowCheetahifyConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Configuration;
+
+namespace SlowCheetahifyConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get the value from the configuration
+            //The Slow Cheetah web.config transform will destermine which value to use from the config files
+            var value = ConfigurationManager.AppSettings["testSetting"];
+
+            Console.WriteLine(value);
+            Console.Read();
+        }
+    }
+}
